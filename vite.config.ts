a62c95a2f71f7e0ff07896d9418e896b570811b5
Nico <nico_204@hotmail.com>
2025-10-24 17:59:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Esto es para desarrollo local (npm run dev)
     port: 3000
+  },
+  preview: {
+    allowedHosts: [
+      'villanet-frontend.onrender.com'
+    ]
   }
 })
